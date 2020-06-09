b8e5e61b995d8f95f2150c1cd9b02d1129b88445
Damien Gaignon <damien.gaignon@gmail.com>
2020-06-09 11:06:42 +0200

    Change init script
    Use a file instead of the last commit to not trigger a database build
    if not needed.

--- init.sh
+++ init.sh
@@ -1,23 +1,27 @@
 #!/bin/bash
 
 link="https://eve-static-data-export.s3-eu-west-1.amazonaws.com/tranquility/sde.zip"
-daterev=$(curl -s -v -X HEAD $link 2>&1 | grep '< Last-Modified:' | awk -F ':' '{print $2":"$3":"$4}')
-currentrev=$(echo $daterev | xargs -0 date +"%s" -d)
-echo 'Current SDE revision of ' $daterev ' -> ' $currentrev
-if [[ $(git log -1 --pretty=%B) == *"$currentrev"* ]]
+resdate=$(curl -s -v -X HEAD $link 2>&1 | grep '< Last-Modified:' | awk -F ':' '{print $2":"$3":"$4}')
+resstamp=$(echo $resdate | xargs -0 date +"%s" -d)
+echo -e "SDE revision of $resdate\n" \
+"\tTimestamp : $resstamp"
+repstamp=$(cat version)
+if [[ $repstamp == *"$resstamp"* ]]
 then
     echo "Already lastest SDE commited"
     exit
 fi
-echo 'Latest commit was :' $(git log -1 --pretty=%B)
-echo 'Building database files for revision' $currentrev
+echo 'Latest commit was :' $repstamp
+echo 'Building database files'
 git config --global user.email "$GH_USER_EMAIL"
 git config --global user.name "$GH_USER_NAME"
 git remote add origin-ssh git@github.com:$GH_REPO
 mkdir -p resources
 python3 db_create.py
 git add resources/eve.db
-git commit -m "Up to date DB with SDE $currentrev"
+echo $resstamp > version
+git add version
+git commit -m "Up to date DB with SDE $resstamp"
 git checkout -b temp
 git checkout -B $TRAVIS_BRANCH temp
 git push --quiet --set-upstream origin-ssh $TRAVIS_BRANCH
